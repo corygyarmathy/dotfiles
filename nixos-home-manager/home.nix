b9a8c6edd753cc1db05b373772e2c4b16964fe8e
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2024-07-29 23:54:42 +0800

    nixos hm: spotify-player colour themes (rose-pine?) not yet working

--- nixos-home-manager/home.nix
+++ nixos-home-manager/home.nix
@@ -710,17 +710,43 @@ in
   };
 
   # Spotify player settings
-  programs.spotify-player.settings = {
-    theme = "default";
-    playback_window_position = "Top";
-    copy_command = {
-      command = "wl-copy";
-      args = [ ];
+  programs.spotify-player = {
+    settings = {
+      theme = "rose-pine";
+      playback_window_position = "Top";
+      copy_command = {
+        command = "wl-copy";
+        args = [ ];
+      };
+      device = {
+        audio_cache = true; # Caches to $APP_CACHE ($HOME/.cache/...)
+        normalization = true; # Enables audio normalisation between songs
+        autoplay = true; # Autoplays similar songs
+      };
     };
-    device = {
-      audio_cache = true; # Caches to $APP_CACHE ($HOME/.cache/...)
-      normalization = true; # Enables audio normalisation between songs
-      autoplay = true; # Autoplays similar songs
+    themes = {
+      # TODO: change colours to rose-pine, currently tokyo-night
+      name = "rose-pine";
+      pallette = {
+        background = "#191724";
+        foreground = "#1f1d2e";
+        black = "#414868";
+        red = "#f7768e";
+        green = "#9ece6a";
+        yellow = "#e0af68";
+        blue = "#2ac3de";
+        magenta = "#bb9af7";
+        cyan = "#7dcfff";
+        white = "#eee8d5";
+        bright_black = "#24283b";
+        bright_red = "#ff4499";
+        bright_green = "#73daca";
+        bright_yellow = "#657b83";
+        bright_blue = "#839496";
+        bright_magenta = "#ff007c";
+        bright_cyan = "#93a1a1";
+        bright_white = "#fdf6e3";
+      };
     };
   };
 
