ce08b8dbcc2cf00d1b30d2759858b9d210c9b162
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2024-07-31 19:38:22 +0800

    nixos: stylix - fix wallpaper file path

--- nixos/hosts/default/configuration.nix
+++ nixos/hosts/default/configuration.nix
@@ -281,7 +281,7 @@ in
     autoEnable = true; # Enables stylix themes for all applications
     base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
     polarity = "dark"; # "light" or "either" - sets light or dark mode
-    image = ../../wallpapers/wallpaper.jpg; # Sets wallpaper, ""s are not required for path
+    image = ../../../wallpapers/wallpaper.jpg; # Sets wallpaper, ""s are not required for path
 
     cursor = {
       package = pkgs.rose-pine-cursor;
