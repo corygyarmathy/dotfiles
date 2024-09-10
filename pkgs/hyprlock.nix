b526ce20fd39518f3ecb2927b7ec0a2806bba88a
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2024-09-10 18:13:01 +0800

    nixos hm, hyprlock, hypridle: updating idle and lock settings

--- pkgs/hyprlock.nix
+++ pkgs/hyprlock.nix
@@ -16,31 +16,57 @@
       enable = true;
       settings = {
         general = {
-          grace = 5;
+          grace = 5; # Seconds before password required
           hide_cursor = true;
         };
 
         background = [
           {
-            path = "../../../wallpapers/wallpaper.jpg";
-            blur_passes = 2;
+            path = "screenshot";
+            blur_passes = 3;
             blur_size = 6;
+            noise = 1.17e-2;
+            contrast = 0.8916;
+            brightness = 0.8172;
           }
         ];
 
         input-field = [
           {
-            size = "250, 60";
-            inner_color = "rgb(91, 96, 120)";
-            outer_color = "rgb(24, 25, 38)";
-            font_color = "rgb(202, 211, 245)";
-            placeholder_text = "";
+            size = "350, 50";
+            outline_thickness = 3;
+            dots_size = 0.3; # Scale of input-field height, 0.2 - 0.8
+            dots_spacing = 0.15; # Scale of dots' absolute size, 0.0 - 1.0
+            dots_center = false;
+            dots_rounding = -1; # -1 default circle, -2 follow input-field rounding
+            outer_color = "rgb(151515)";
+            inner_color = "rgb(200, 200, 200)";
+            font_color = "rgb(10, 10, 10)";
+            fade_on_empty = "true";
+            fade_timeout = "5000"; # Milliseconds before fade_on_empty is triggered.
+            placeholder_text = "<i>Input Password...</i>"; # Text rendered in the input box when it's empty.
+            hide_input = "false";
+            rounding = -1; # -1 means complete rounding (circle/oval)
+            check_color = "rgb(204, 136, 34)";
+            fail_color = "rgb(204, 34, 34)"; # if authentication failed, changes outer_color and fail message color
+            fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>"; # can be set to empty
+            fail_timeout = 1000; # milliseconds before fail_text and fail_color disappears
+            fail_transition = 200; # transition time in ms between normal outer_color and fail_color
+            capslock_color = -1;
+            numlock_color = -1;
+            bothlock_color = -1; # when both locks are active. -1 means don't change outer color (same for above)
+            invert_numlock = false; # change color if numlock is off
+            swap_font_color = false; # see below
+
+            position = "0, -15";
+            halign = "center";
+            valign = "center";
           }
         ];
 
         label = [
           {
-            text = "Hello";
+            text = "Hello, $DESC";
             # color = "rgba(${hexToRgb colours.text}, 1.0)";
             # font_family = theme.fonts.default.name;
             font_size = 64;
@@ -74,12 +100,17 @@
         listener = [
           {
             timeout = 300; # In seconds. 300s is 5m
-            on-timeout = "${lib.getExe pkgs.hyprlock}";
+            on-timeout = "${lib.getExe pkgs.hyprlock}"; # Lock
           }
           {
             timeout = 305;
-            on-timeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
-            on-resume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+            on-timeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off"; # Screen off
+            on-resume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on"; # Screen on
+          }
+          {
+            # TODO: make it so I can wake from suspend with external keyboard, don't have to open laptop
+            timeout = 1800; # 30min
+            on-timeout = "systemctl suspend"; # suspend pc
           }
         ];
       };
