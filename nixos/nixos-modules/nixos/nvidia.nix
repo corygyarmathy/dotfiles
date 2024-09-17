503a75fd32573f064be11da295aafb17c500b940
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2024-09-17 19:58:15 +0800

    nixos: disable nvidia powerManagement option - sleep was crashing every time, now seems somewhat improved

--- nixos/nixos-modules/nixos/nvidia.nix
+++ nixos/nixos-modules/nixos/nvidia.nix
@@ -38,7 +38,8 @@
       # Enable this if you have graphical corruption issues or application crashes after waking
       # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
       # of just the bare essentials.
-      powerManagement.enable = true;
+      # NOTE: when true, seemed to crash display manager when resuming from sleep
+      powerManagement.enable = false;
 
       # Fine-grained power management. Turns off GPU when not in use.
       # Experimental and only works on modern Nvidia GPUs (Turing or newer).
