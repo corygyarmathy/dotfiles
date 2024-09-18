ede09294973165ee015d135fa2030e76a6db0d7e
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2024-09-18 19:47:27 +0800

    nixos: comments

--- nixos/hosts/xps15/configuration.nix
+++ nixos/hosts/xps15/configuration.nix
@@ -97,8 +97,8 @@ in
         flake-registry = "";
         # Workaround for https://github.com/NixOS/nix/issues/9574
         nix-path = config.nix.nixPath;
-        substituters = [ "https://hyprland.cachix.org" ]; # Required for Hyprland, https://wiki.hyprland.org/Nix/Cachix/
-        trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ]; # Required for Hyprland
+        substituters = [ "https://hyprland.cachix.org" ]; # Req. for Hyprland, https://wiki.hyprland.org/Nix/Cachix/
+        trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ]; # Req. for Hyprland
       };
       # Opinionated: disable channels
       channel.enable = false;
