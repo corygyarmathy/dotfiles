b3fc8a5e8c94a933f1088c07fe35d865a9bcf3de
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2024-09-22 21:48:35 +0800

    ddc.nix: comment out unfinished code

--- nixos/nixos-modules/nixos/ddc.nix
+++ nixos/nixos-modules/nixos/ddc.nix
@@ -13,22 +13,22 @@
   config = lib.mkIf config.cg.ddc.enable {
     hardware.i2c.enable = true; # req. for ddcutil (monitor brightness control)
 
-    systemd.timers."hello-world" = {
-      wantedBy = [ "timers.target" ];
-      timerConfig = {
-        OnCalendar = "*-*-* 4:00:00"; # *-*-* = every day, 00:00:00 time, 24-h time
-        Unit = "hello-world.service";
-      };
-    };
-
-    systemd.services."hello-world" = {
-      script = ''
-        ddcutil setvcp 10 55 --display2
-      '';
-      serviceConfig = {
-        Type = "oneshot";
-      };
-    };
+    # systemd.timers."hello-world" = {
+    #   wantedBy = [ "timers.target" ];
+    #   timerConfig = {
+    #     OnCalendar = "*-*-* 4:00:00"; # *-*-* = every day, 00:00:00 time, 24-h time
+    #     Unit = "hello-world.service";
+    #   };
+    # };
+    #
+    # systemd.services."hello-world" = {
+    #   script = ''
+    #     ddcutil setvcp 10 55 --display2
+    #   '';
+    #   serviceConfig = {
+    #     Type = "oneshot";
+    #   };
+    # };
     environment.systemPackages = with pkgs; [
       ddcutil # Display management UI
       ddcui # Dispay management tool
