14ab983cf663383b57c9eb4db38092a6b00565f9
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2024-11-22 17:34:51 +0800

    fix(hyprland): comment out depreciated shadow options

--- pkgs/hyprland.nix
+++ pkgs/hyprland.nix
@@ -111,12 +111,12 @@ in
             popups_ignorealpha = 0.2;
           };
 
-          drop_shadow = true;
-          shadow_ignore_window = true;
-          shadow_offset = "0 15";
-          shadow_range = 100;
-          shadow_render_power = 2;
-          shadow_scale = 0.97;
+          # drop_shadow = true;
+          # shadow_ignore_window = true;
+          # shadow_offset = "0 15";
+          # shadow_range = 100;
+          # shadow_render_power = 2;
+          # shadow_scale = 0.97;
           # "col.shadow" = "rgba(00000055)";
         };
 
