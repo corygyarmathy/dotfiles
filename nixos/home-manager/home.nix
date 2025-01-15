e1938a0c841925fd2ed1155d1ed381bfd4bb2645
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-01-15 08:51:42 +0800

    feat(zellij): auto-launch when init bash shell

--- nixos/home-manager/home.nix
+++ nixos/home-manager/home.nix
@@ -87,6 +87,19 @@
   # Bash config
   programs.bash = {
     enable = true; # Req. for starship to work
+    initExtra = ''
+      # Launch zellij on shell open
+      if [[ -z "$ZELLIJ" ]]; then
+          if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
+              zellij attach -c
+          else
+              zellij
+          fi
+
+          if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
+              exit
+          fi
+      fi'';
   };
 
   # Git config
