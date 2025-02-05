9a03ea879b72c5e1b646620c811f1de910a4d89c
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-02-05 18:38:54 +0800

    feat: cleaned up bookbot code

--- main.py
+++ main.py
@@ -1,41 +1,99 @@
+import sys
+from collections import Counter
+
+
 def main():
     book_path: str = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    word_count = get_num_words(text)
-    chars_dict = get_alpha_chars_dict(text)
-    sorted_chars_dict = dict(
-        sorted(chars_dict.items(), key=lambda x: x[1], reverse=True)
-    )
-    print(f"--- Begin report of {book_path} ---")
+
+    # get book text
+    try:
+        text: str = get_book_text(book_path)
+    except (
+        BookFileNotFoundError,
+        BookFilePermissionError,
+        BookFileIsADirectoryError,
+    ) as e:  # Specific file error
+        print(f"Error: {e}")
+        sys.exit(1)  # Exit with error status
+    except BookFileError as e:  # General file error
+        print(f"Error: {e}")
+        sys.exit(1)  # Exit with error status
+    except BookError as e:  # Unhandled, general error
+        print("Unhandled error!")
+        print(f"Error: {e}")
+        sys.exit(1)  # Exit with error status
+
+    word_count: int = count_words(text)
+    chars_count: Counter[str] = count_alphabetic_characters(text)
+
+    print_report(book_path, word_count, chars_count)
+
+
+def print_report(path: str, word_count: int, chars_count: Counter[str]):
+    """Format and print the report of how many times each character occurs"""
+    print(f"--- Begin report of {path} ---")
     print(f"{word_count} words found in the document")
     print()
-    for char in sorted_chars_dict:
-        print(f" The '{char}' character was found {sorted_chars_dict[char]} times")
+    for char, count in chars_count.most_common():
+        print(f" The '{char}' character was found {count} times")
     print("--- End report ---")
 
 
-def get_book_text(path: str):
-    with open(path) as f:
-        return f.read()
+def get_book_text(path: str) -> str:
+    """Read the file at *path*, return the file's contents"""
+    try:
+        with open(path) as open_file:
+            return open_file.read()
+    except FileNotFoundError:
+        raise BookFileNotFoundError(f"No file found at {path}")
+    except PermissionError:
+        raise BookFilePermissionError(f"Cannot read file at {path}. Check permissions.")
+    except IsADirectoryError:
+        raise BookFileIsADirectoryError(f"Path {path} is a directory, not a file.")
+    except Exception as err:
+        raise BookError(f"Unexpected {err=}, {type(err)=}")
 
 
-def get_num_words(text: str) -> int:
+def count_words(text: str) -> int:
+    """Count the number of words in the text"""
     words = text.split()
 
     return len(words)
 
 
-def get_alpha_chars_dict(text: str) -> dict[str, int]:
-    lowered_string = text.lower()  # Only counting lowercase characters
+def count_alphabetic_characters(text: str) -> Counter[str]:
+    """Count the number of alphabetic characters in the text, converting them all to lowercase"""
+    return Counter(char for char in text.lower() if char.isalpha())
 
-    char_count: dict[str, int] = {}
-    for char in lowered_string:
-        if char.isalpha():
-            if char in char_count:
-                char_count[char] += 1
-            else:
-                char_count[char] = 1
-    return char_count
+
+class BookError(Exception):
+    """Base exception for all book errors"""
+
+    pass
+
+
+class BookFileError(BookError):
+    """Base exception for all book file operations"""
+
+    pass
+
+
+class BookFileNotFoundError(BookFileError):
+    """Raised when book file is not found"""
+
+    pass
+
+
+class BookFilePermissionError(BookFileError):
+    """Raised when program does not have permission to read the book file"""
+
+    pass
+
+
+class BookFileIsADirectoryError(BookFileError):
+    """Raised when the supplied path points to a directory, not a file as required"""
+
+    pass
 
 
 main()
