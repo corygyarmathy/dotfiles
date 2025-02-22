9c7238bc229af01a0388c7afc6b3c5a2e012ee9d
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-02-22 18:23:54 +0800

    feat(nvim): migrate config to submodule

--- pkgs/nvim/init.lua
+++
@@ -1,9 +0,0 @@
--- Set up the required lua files (in the 'Lua' folder)
-require 'options'
-require 'keymap'
-require 'autocommands'
-require 'lazy-bootstrap' -- Install Lazy (plugin manager)
-require 'lazy-plugins' -- Install the required plugins through Lazy
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
--- pkgs/nvim/lua/kickstart/plugins/vim-sleuth.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-}
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
