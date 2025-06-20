06416aa8a538e935c4e0c4468a283dce9bbb16e0
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-06-20 17:24:11 +0800

    fix: remove legacy udev rules
    - Incorrectly formatted, causing build failures
    - No longer needed, as the firmware on my keyboard has been updated

--- nixos/nixos-modules/nixos/ergodox.nix
+++ nixos/nixos-modules/nixos/ergodox.nix
@@ -19,16 +19,6 @@
       KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664"
       KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664"
 
-      # Legacy rules for live training over webusb (Not needed for firmware v21+)
-        # Rule for all ZSA keyboards
-        SUBSYSTEM=="usb", ATTR{idVendor}=="3297"
-        # Rule for the Moonlander
-        SUBSYSTEM=="usb", ATTR{idVendor}=="3297", ATTR{idProduct}=="1969"
-        # Rule for the Ergodox EZ
-        SUBSYSTEM=="usb", ATTR{idVendor}=="feed", ATTR{idProduct}=="1307"
-        # Rule for the Planck EZ
-        SUBSYSTEM=="usb", ATTR{idVendor}=="feed", ATTR{idProduct}=="6060"
-
       # Wally Flashing rules for the Ergodox EZ
       ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
       ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
