d197c0f33f4c8cc4a020495b8ff8992c27598e21
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-11-20 21:26:51 +0800

    feat(nvim): update snacks plugin config
    - idk what I did here exactly, think it was for images

--- pkgs/nvim/lua/plugins/snacks.lua
+++ pkgs/nvim/lua/plugins/snacks.lua
@@ -1,22 +1,22 @@
 return {
-  "folke/snacks.nvim",
-  ---@type snacks.Config
-  opts = {
-    picker = {
-      hidden = true,
-      ignored = true,
-      sources = {
-        files = {
-          hidden = true,
-          -- ignored = true,
-        },
-      },
-    },
-    scroll = {
-      enabled = false,
-    },
-  },
+	"folke/snacks.nvim",
+	---@type snacks.Config
+	opts = {
 		image = {
 			img_dirs = { "img", "images", "assets", "static", "public", "media", "attachments", "files", "Files" },
 		},
+		picker = {
+			hidden = true,
+			ignored = true,
+			sources = {
+				files = {
+					hidden = true,
+					-- ignored = true,
+				},
+			},
+		},
+		scroll = {
+			enabled = false,
+		},
+	},
 }
