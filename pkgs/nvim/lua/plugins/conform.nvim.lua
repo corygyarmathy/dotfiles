758abe99f529f558e09f1cbc883575d48de4e2fd
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-11-20 21:23:05 +0800

    fix(nvim): fix conform markdown formatting

--- pkgs/nvim/lua/plugins/conform.nvim.lua
+++ pkgs/nvim/lua/plugins/conform.nvim.lua
@@ -1,17 +1,16 @@
 return {
 	"stevearc/conform.nvim",
 	opts = {
-		formatters_by_ft = {
-			markdown = { "markdownlint-cli2" }, -- Replace prettier with markdownlint
-		},
 		formatters = {
 			["markdownlint-cli2"] = {
+				command = "markdownlint-cli2",
 				args = {
 					"--config",
 					vim.fn.expand("$HOME/.config/nvim/lua/plugins/.markdownlint-cli2.yaml"),
 					"--fix",
 					"$FILENAME",
 				},
+				stdin = false,
 			},
 		},
 	},
