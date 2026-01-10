65113ab40fe0d0cf35e5499dfe02facbcedfa4fc
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2026-01-10 09:45:53 +0800

    feat(arr): add cleanuparr to cleanup stalled/failed downloads

---
+++ modules/services/cleanuparr.nix
@@ -0,0 +1,42 @@
+# Cleanuparr - Clean up stalled/failed downloads
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.cg.service.cleanuparr;
+  configPath = "/srv/arr/cleanuparr";
+in
+{
+  options.cg.service.cleanuparr.enable = lib.mkEnableOption "Cleanuparr stalled download cleanup";
+
+  config = lib.mkIf cfg.enable {
+    systemd.tmpfiles.rules = [
+      "d ${configPath} 0775 coryg media -"
+    ];
+
+    virtualisation.oci-containers.containers.cleanuparr = {
+      image = "ghcr.io/flmedicern/cleanuparr:latest";
+      volumes = [
+        "${configPath}:/config"
+      ];
+      environment = {
+        TZ = config.time.timeZone;
+      };
+      ports = [ "5000:5000" ];
+      extraOptions = [
+        "--pull=newer"
+        "--network=arr-network"
+      ];
+    };
+
+    systemd.services.podman-cleanuparr = {
+      after = [ "podman-network-arr.service" ];
+      requires = [ "podman-network-arr.service" ];
+    };
+
+    networking.firewall.allowedTCPPorts = [ 5000 ];
+  };
+}
