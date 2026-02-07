93890fcfe1b5f90946528cd31489fbb503747825
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2026-02-07 19:26:11 +0800

    fix(argtable2): fetch from GitHub

--- packages/argtable2/default.nix
+++ packages/argtable2/default.nix
@@ -1,21 +1,28 @@
 {
   lib,
   stdenv,
-  fetchurl,
+  fetchFromGitHub,
+  autoreconfHook,
 }:
 
 stdenv.mkDerivation rec {
   pname = "argtable2";
   version = "2.13";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/argtable/argtable2-${version}.tar.gz";
-    hash = "sha256-f3YW5nCGmHcnNFEkNNiXZQUT1B0+5ey8R56XBFbLYIo=";
+  src = fetchFromGitHub {
+    owner = "jonathanmarvens";
+    repo = "argtable2";
+    rev = "argtable-${version}";
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # We'll get this from the error
   };
+
+  nativeBuildInputs = [
+    autoreconfHook
+  ];
 
   meta = with lib; {
     description = "ANSI C library for parsing GNU-style command-line options";
-    homepage = "http://argtable.sourceforge.net/";
+    homepage = "https://github.com/jonathanmarvens/argtable2";
     license = licenses.lgpl2Plus;
     platforms = platforms.unix;
     maintainers = [ ];
