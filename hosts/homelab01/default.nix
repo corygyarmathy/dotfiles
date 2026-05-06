1e9a5e81cf0141e64990adf673831dc125e93c2e
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2026-05-06 21:54:20 +0800

    feat(maintainerr): disable API rate limit - needs to scan entire Jellyfin library

--- hosts/homelab01/default.nix
+++ hosts/homelab01/default.nix
@@ -215,7 +215,7 @@
           subdomain = "maintainerr";
           port = 6246;
           localOnly = true;
-          rateLimitProfile = "media"; # Frequent API calls
+          rateLimitProfile = "none"; # Scans entire Jellyfin library
         };
       };
     };
