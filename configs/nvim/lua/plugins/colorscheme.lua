1da6c997f0644e814a0dc18b94eb8d483ae12bd4
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2026-05-21 16:37:35 +0800

    feat(nvim): make aspects of theme transparent

--- configs/nvim/lua/plugins/colorscheme.lua
+++ configs/nvim/lua/plugins/colorscheme.lua
@@ -7,9 +7,39 @@ return {
 	{
 		"rebelot/kanagawa.nvim",
 		enabled = true,
+		lazy = false,
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		opts = {
-			transparent_background = true,
+			transparent = true,
+			colors = {
+				theme = {
+					all = {
+						ui = {
+							-- Remove background colour of gutter, making it transparent
+							bg_gutter = "none",
+						},
+					},
+				},
+			},
+			overrides = function(colors)
+				local theme = colors.theme
+				return {
+					-- Remove background colour of floating windows, making them transparent
+					NormalFloat = { bg = "none" },
+					FloatBorder = { bg = "none" },
+					FloatTitle = { bg = "none" },
+
+					-- Save an hlgroup with dark background and dimmed foreground
+					-- so that you can use it where your still want darker windows.
+					-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+					NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+					-- Popular plugins that open floats will link to NormalFloat by default;
+					-- set their background accordingly if you wish to keep them dark and borderless
+					LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+					MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+				}
+			end,
 		},
 	},
 	-- Configure LazyVim to load theme
