c41a69521b40c08a38883b233be55f3eda059dc1
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2026-06-18 22:42:38 +0800

    feat(nvim): no-neck-pain width; 100 -> 95

--- configs/nvim/lua/plugins/no-neck-pain.lua
+++ configs/nvim/lua/plugins/no-neck-pain.lua
@@ -3,7 +3,7 @@ return {
 	version = "*",
 	cmd = { "NoNeckPain", "NoNeckPainResize" },
 	opts = {
-		width = 100, -- your max content width; tune 80–100
+		width = 95, -- your max content width; tune 80–100
 		autocmds = {
 			enableOnVimEnter = false, -- we enable it per-note instead (below)
 			skipEnteringNoNeckPainBuffer = true,
